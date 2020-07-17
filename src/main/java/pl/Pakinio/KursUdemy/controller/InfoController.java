0499c2f8e66cf1459409460439226cc4688e7669
s18535 <s18535@pjwstk.edu.pl>
2020-07-17 20:34:17 +0200

    End Commit
    Signed-off-by: s18535 <s18535@pjwstk.edu.pl>

--- src/main/java/pl/Pakinio/KursUdemy/controller/InfoController.java
+++ src/main/java/pl/Pakinio/KursUdemy/controller/InfoController.java
@@ -3,10 +3,13 @@ package pl.Pakinio.KursUdemy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.Pakinio.KursUdemy.TaskConfigurationProperties;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/info")
@@ -22,11 +25,13 @@ public class InfoController {
         this.myProp = myProp;
     }
 
+    @Secured("ROLE_ADMIN")
     @GetMapping("/url")
     String url(){
         return dataSource.getUrl();
     }
 
+    @RolesAllowed({"ROLE_USER","ROLE_ADMIN"})
     @GetMapping("/prop")
     boolean myProp(){
         return myProp.getTemplate().isAllowMultipleTasks();
---
+++ src/main/java/pl/Pakinio/KursUdemy/security/SsoController.java
@@ -0,0 +1,17 @@
+package pl.Pakinio.KursUdemy.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class SsoController {
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "index";
+    }
+}
